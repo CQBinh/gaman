8569ff9a0613bda306a652c759935c9430f7a9a0
Cao Quang Binh <binhcq@asiantech.vn>
2016-01-14 17:20:06 +0700

    [Update] Un-comment all linter

--- bin/checker.sh
+++ bin/checker.sh
@@ -25,59 +25,59 @@ cat rubocop.xml \
   | saddler report \
     --require saddler/reporter/github \
     --reporter $REPORTER
-#
-# echo "***********************"
-# echo "* Rails Best Pratices *"
-# echo "***********************"
-# rails_best_practices -f xml
-#
-# cat rails_best_practices_output.xml \
-#   | checkstyle_filter-git diff origin/master \
-#   | saddler report \
-#     --require saddler/reporter/github \
-#     --reporter $REPORTER
-#
-# echo "********************"
-# echo "* Brakeman         *"
-# echo "********************"
-# brakeman -o brakeman.json
-# cat brakeman.json \
-#   | brakeman_translate_checkstyle_format translate \
-#   | checkstyle_filter-git diff origin/master \
-#   | saddler report \
-#     --require saddler/reporter/github \
-#     --reporter $REPORTER
-#
-#
-# echo "********************"
-# echo "* Reek             *"
-# echo "********************"
-# reek app --format xml > reek.xml
-# cat reek.xml \
-#   | checkstyle_filter-git diff origin/master \
-#   | saddler report \
-#     --require saddler/reporter/github \
-#     --reporter $REPORTER
-#
-# echo "********************"
-# echo "* ESLint           *"
-# echo "********************"
-# eslint app/assets/javascripts/ -f checkstyle -o eslint.xml
-# cat eslint.xml \
-#   | checkstyle_filter-git diff origin/master \
-#   | saddler report \
-#     --require saddler/reporter/github \
-#     --reporter $REPORTER
-#
-# echo "********************"
-# echo "* SCSS-Lint        *"
-# echo "********************"
-# scss-lint --require=scss_lint_reporter_checkstyle --format=Checkstyle -o scsslint.xml
-# cat scsslint.xml \
-#   | checkstyle_filter-git diff origin/master \
-#   | saddler report \
-#     --require saddler/reporter/github \
-#     --reporter $REPORTER
+
+echo "***********************"
+echo "* Rails Best Pratices *"
+echo "***********************"
+rails_best_practices -f xml
+
+cat rails_best_practices_output.xml \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
+    --require saddler/reporter/github \
+    --reporter $REPORTER
+
+echo "********************"
+echo "* Brakeman         *"
+echo "********************"
+brakeman -o brakeman.json
+cat brakeman.json \
+  | brakeman_translate_checkstyle_format translate \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
+    --require saddler/reporter/github \
+    --reporter $REPORTER
+
+
+echo "********************"
+echo "* Reek             *"
+echo "********************"
+reek app --format xml > reek.xml
+cat reek.xml \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
+    --require saddler/reporter/github \
+    --reporter $REPORTER
+
+echo "********************"
+echo "* ESLint           *"
+echo "********************"
+eslint app/assets/javascripts/ -f checkstyle -o eslint.xml
+cat eslint.xml \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
+    --require saddler/reporter/github \
+    --reporter $REPORTER
+
+echo "********************"
+echo "* SCSS-Lint        *"
+echo "********************"
+scss-lint --require=scss_lint_reporter_checkstyle --format=Checkstyle -o scsslint.xml
+cat scsslint.xml \
+  | checkstyle_filter-git diff origin/master \
+  | saddler report \
+    --require saddler/reporter/github \
+    --reporter $REPORTER
 
 echo "********************"
 echo "* save outputs     *"
@@ -86,8 +86,8 @@ LINT_RESULT_DIR="$CIRCLE_ARTIFACTS/lint"
 
 mkdir "$LINT_RESULT_DIR"
 cp -v "rubocop.xml" "$LINT_RESULT_DIR/"
-# cp -v "rails_best_practices_output.xml" "$LINT_RESULT_DIR/"
-# cp -v "brakeman.json" "$LINT_RESULT_DIR/"
-# cp -v "reek.xml" "$LINT_RESULT_DIR/"
-# cp -v "eslint.xml" "$LINT_RESULT_DIR/"
-# cp -v "scsslint.xml" "$LINT_RESULT_DIR/"
+cp -v "rails_best_practices_output.xml" "$LINT_RESULT_DIR/"
+cp -v "brakeman.json" "$LINT_RESULT_DIR/"
+cp -v "reek.xml" "$LINT_RESULT_DIR/"
+cp -v "eslint.xml" "$LINT_RESULT_DIR/"
+cp -v "scsslint.xml" "$LINT_RESULT_DIR/"
